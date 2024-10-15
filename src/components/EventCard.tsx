0165c9afb6596091b46c8648e9d657dc91b7affd
Vansh Vasishtha <vansh19092002@gmail.com>
2024-10-15 19:11:01 +0530

    Event Card Created with Format descriptions

---
+++ src/components/EventCard.tsx
@@ -0,0 +1,59 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { formatEventDescription } from "@/lib/formatters";
+import { cn } from "@/lib/utils";
+import { CopyEventButton } from "@/components/CopyEventButton";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+type EventCardProps = {
+  id: string;
+  isActive: boolean;
+  name: string;
+  description: string | null;
+  durationInMinutes: number;
+  clerkUserId: string;
+};
+
+export const EventCard: React.FC<EventCardProps> = ({
+  id,
+  isActive,
+  name,
+  description,
+  durationInMinutes,
+  clerkUserId,
+}: EventCardProps) => {
+  return (
+    <Card className={cn("flex flex-col", !isActive && "border-secondary/50")}>
+      <CardHeader className={cn(!isActive && "opacity-50")}>
+        <CardTitle>{name}</CardTitle>
+        <CardDescription>
+          {formatEventDescription(durationInMinutes)}
+        </CardDescription>
+      </CardHeader>
+      {description != null && (
+        <CardContent className={cn(!isActive && "opacity-50")}>
+          {description}
+        </CardContent>
+      )}
+      <CardFooter className="flex justify-end gap-2 mt-auto">
+        {isActive && (
+          <CopyEventButton
+            variant="outline"
+            eventId={id}
+            clerkUserId={clerkUserId}
+          />
+        )}
+        <Button asChild>
+          <Link href={`/events/${id}/edit`}>Edit</Link>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
