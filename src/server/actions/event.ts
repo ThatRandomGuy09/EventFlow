74f8b22cb0125df07a5e5bb32d4ffc9c1560d6b6
Vansh Vasishtha <vansh19092002@gmail.com>
2024-10-15 19:11:34 +0530

    Added Server Actions for Update and Delete

--- src/server/actions/event.ts
+++ src/server/actions/event.ts
@@ -3,6 +3,7 @@ import { db } from "@/drizzle/db";
 import { EventTables } from "@/drizzle/schema";
 import { eventFormSchema } from "@/schema/events";
 import { auth } from "@clerk/nextjs/server";
+import { and, eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 import "use-server";
 import { z } from "zod";
@@ -19,5 +20,48 @@ export async function createEvent(
   }
 
   await db.insert(EventTables).values({ ...data, clerkUserId: userId });
+  redirect("/events");
+}
+
+export async function updateEvent(
+  id: string,
+  unsafeData: z.infer<typeof eventFormSchema>
+): Promise<{ error: boolean } | undefined> {
+  const { userId } = auth();
+  const { success, data } = eventFormSchema.safeParse(unsafeData);
+
+  if (!success || userId == null) {
+    return { error: true };
+  }
+
+  const { rowCount } = await db
+    .update(EventTables)
+    .set({ ...data })
+    .where(and(eq(EventTables.id, id), eq(EventTables.clerkUserId, userId)));
+
+  if (rowCount === 0) {
+    return { error: true };
+  }
+
+  redirect("/events");
+}
+
+export async function deleteEvent(
+  id: string
+): Promise<{ error: boolean } | undefined> {
+  const { userId } = auth();
+
+  if (userId == null) {
+    return { error: true };
+  }
+
+  const { rowCount } = await db
+    .delete(EventTables)
+    .where(and(eq(EventTables.id, id), eq(EventTables.clerkUserId, userId)));
+
+  if (rowCount === 0) {
+    return { error: true };
+  }
+
   redirect("/events");
 }
