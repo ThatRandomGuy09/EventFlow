cee92ecc8dd6f53e91ae76c5ee236cedda5bef55
Vansh Vasishtha <vansh19092002@gmail.com>
2024-10-19 21:05:33 +0530

    Booking page Building....

--- src/app/(unprotected)/book/[clerkUserId]/[eventId]/page.tsx
+++ src/app/(unprotected)/book/[clerkUserId]/[eventId]/page.tsx
@@ -1,7 +1,44 @@
-export default function BookEventPage() {
-    return(
-        <h1>
-        Book
-        </h1>
-    )
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { db } from "@/drizzle/db";
+import { notFound } from "next/navigation";
+import { clerkClient } from "@clerk/nextjs/server";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  addMonths,
+  eachMinuteOfInterval,
+  endOfDay,
+  roundToNearestMinutes,
+} from "date-fns";
+import Link from "next/link";
+
+export const revalidate = 0;
+export default async function BookEventPage({
+  params: { clerkUserId, eventId },
+}: {
+  params: { clerkUserId: string; eventId: string };
+}) {
+  const event = await db.query.EventTables.findFirst({
+    where: ({ clerkUserId: userIdCol, isActive, id }, { eq, and }) =>
+      and(eq(isActive, true), eq(userIdCol, clerkUserId), eq(id, eventId)),
+  });
+  if (event == null) return notFound();
+
+  const calendarUser = await clerkClient().users.getUser(clerkUserId);
+
+  const startDate = roundToNearestMinutes(new Date(), {
+    nearestTo: 15,
+    roundingMethod: "ceil",
+  });
+  const endDate = endOfDay(addMonths(startDate, 2));
+
+  return <></>;
 }
